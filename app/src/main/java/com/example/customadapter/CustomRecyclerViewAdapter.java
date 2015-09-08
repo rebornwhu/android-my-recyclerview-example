4b8f35cfb67140865a4fe401a990536104c5d991
rebornwhu <rebornwhu@gmail.com>
2015-09-07 22:36:54 -0700

    Implement custom recyclerviewadapter

---
+++ app/src/main/java/com/example/customadapter/CustomRecyclerViewAdapter.java
@@ -0,0 +1,68 @@
+package com.example.customadapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.chinesechairmen.R;
+
+import java.util.ArrayList;
+
+public class CustomRecyclerViewAdapter extends RecyclerView.Adapter<CustomRecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<Leader> leaders;
+
+    public CustomRecyclerViewAdapter(ArrayList<Leader> leaders) {
+        this.leaders = leaders;
+    }
+
+    @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        /**
+         * xl The difference between listview view holder is that: here inflated root custom view
+         * becomes a parameter of the view holder's constructor, child view lookup is conducted
+         * inside view holder's constructor.
+         */
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_view, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.getNameTv().setText(leaders.get(position).getName());
+        holder.getImageView().setImageResource(leaders.get(position).getImage());
+        holder.getCommentTv().setText(leaders.get(position).getComment());
+    }
+
+    @Override public int getItemCount() {
+        return leaders.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView nameTv;
+        private final ImageView imageView;
+        private final TextView commentTv;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            nameTv = (TextView) itemView.findViewById(R.id.textView1);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView1);
+            commentTv = (TextView) itemView.findViewById(R.id.textView2);
+        }
+
+        public TextView getNameTv() {
+            return nameTv;
+        }
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+
+        public TextView getCommentTv() {
+            return commentTv;
+        }
+    }
+
+}
--- app/src/main/java/com/example/customadapter/MainActivity.java
+++ app/src/main/java/com/example/customadapter/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.customadapter;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends ListActivity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		CustomAdapter customAdapter = new CustomAdapter(this);
-		setListAdapter(customAdapter);
-	}
+import com.example.chinesechairmen.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.recyclerView) RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Resources res = getResources();
+
+        List<Leader> leaders = new ArrayList<Leader>();
+        String[] names = res.getStringArray(R.array.names);
+        TypedArray images = res.obtainTypedArray(R.array.images);
+        String[] comments = res.getStringArray(R.array.comments);
+        for (int i = 0; i < 5; i++) {
+            leaders.add(new Leader(names[i], images.getResourceId(i, -1), comments[i]));
+            /**"-1" is defValue - Value to return if the attribute is not defined or not a
+             resource. Possibly it will set the imageview blank if it has to use the default -1*/
+
+            /**The following codes won't work
+             images[i] = imagesArray.getResourceId(i, -1);
+             leaders.add(new Leader(names[i], images[i], comments[i]) );*/
+        }
+
+
+
+    }
 }
