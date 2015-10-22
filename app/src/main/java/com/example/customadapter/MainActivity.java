aa5acf7f15dce57f1a9aa48c4ef99bf5594fd652
rebornwhu <rebornwhu@gmail.com>
2015-10-21 19:23:56 -0700

    Draw text on swipe item

--- app/src/main/java/com/example/customadapter/MainActivity.java
+++ app/src/main/java/com/example/customadapter/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.customadapter;
 
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.TypedValue;
+import android.view.View;
 
 import com.example.chinesechairmen.R;
 
@@ -20,7 +25,11 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.recyclerView) RecyclerView recyclerView;
 
     private CustomRecyclerViewAdapter adapter;
-    private ArrayList<Leader> leaders;
+
+    private Paint bgPaint;
+    private Paint textPaint;
+    private float swipeHintTextSize;
+    private float swipeTextUnitMargin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +40,22 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        leaders = createDummyData();
-        adapter = new CustomRecyclerViewAdapter(leaders);
+        adapter = new CustomRecyclerViewAdapter(createDummyData());
         recyclerView.setAdapter(adapter);
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
         itemTouchHelper.attachToRecyclerView(recyclerView);
+
+        bgPaint = new Paint();
+        bgPaint.setColor(ContextCompat.getColor(getBaseContext(), R.color.green));
+        textPaint = new Paint();
+
+        Resources res = getResources();
+        swipeHintTextSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, res.getDisplayMetrics());
+        textPaint.setTextSize(swipeHintTextSize);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setColor(ContextCompat.getColor(getBaseContext(), R.color.white));
+
+        swipeTextUnitMargin = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, res.getDisplayMetrics());
     }
 
     private ArrayList<Leader> createDummyData() {
@@ -66,12 +86,34 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-            int position = viewHolder.getLayoutPosition();
-            adapter.getLeaders().remove(position);
-            adapter.notifyDataSetChanged();
+            if (direction == ItemTouchHelper.RIGHT) {
+                int position = viewHolder.getLayoutPosition();
+                adapter.getLeaders().remove(position);
+                adapter.notifyDataSetChanged();
+            }
         }
 
+        @Override
+        public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                View view = viewHolder.itemView;
 
+                if (dX > 0) {
+                    c.drawRect((float) view.getLeft(), (float) view.getTop(), dX, (float) view.getBottom(), bgPaint);
+                    float textYPosition = (float)(view.getTop() + view.getBottom()) / 2 + swipeHintTextSize / 2;
+                    c.drawText("Yes It's Me", swipeTextUnitMargin, textYPosition, textPaint);
+                }
+                else {
+                    c.drawRect((float)view.getRight() + dX, (float)view.getTop(), (float)view.getRight(), (float)view.getBottom(), bgPaint);
+                    float textYPosition = (float)(view.getTop() + view.getBottom()) / 2 + swipeHintTextSize / 2;
+                    c.drawText("More Info", (float) view.getRight() + dX + swipeTextUnitMargin, textYPosition, textPaint);
+                }
+            }
+
+            // xl: Must have the following line, otherwise it won't execute
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
     };
 
 }
